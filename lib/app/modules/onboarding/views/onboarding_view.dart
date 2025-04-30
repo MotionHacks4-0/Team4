1348304d9a5d318c504a3597d498e0d7391e9f51
rahmahaisyah <rahmahasyh@example.com>
2025-04-30 09:20:20 +0700

    added onboarding screen

--- lib/app/modules/onboarding/views/onboarding_view.dart
+++ lib/app/modules/onboarding/views/onboarding_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+import '../../../routes/app_pages.dart';
 import '../controllers/onboarding_controller.dart';
 
 class OnboardingView extends GetView<OnboardingController> {
@@ -11,65 +12,137 @@ class OnboardingView extends GetView<OnboardingController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(
         children: [
           PageView(
             controller: controller.indicator,
+            onPageChanged: (value) {
+              controller.page.value = value;
+              print(controller.page.value);
+
+              if (controller.page.value == 2) {
+                Future.delayed(const Duration(seconds: 2), () {
+                  Get.offAllNamed(Routes.HOME);
+                });
+              }
+            },
             children: [
-              Container(
-                height: Get.height * 0.65,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/onboard1.png'),
-                        scale: 1.75)),
+              //start page
+              OnBoardingWidgets(),
+              OnBoardingWidgets(),
+              OnBoardingWidgets(),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: 32.0,
+                left: 24.0,
+                right: 24.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Skip button
+                  GestureDetector(
+                    onTap: () {
+                      controller.indicator.jumpToPage(3);
+                    },
+                    child: Text(
+                      "Skip",
+                      style: Style.headLineStyle5,
+                    ),
+                  ),
+
+                  // SmoothPageIndicator
+                  Obx(
+                    () => Container(
+                      child: controller.page.value != 3
+                          ? SmoothPageIndicator(
+                              controller: controller.indicator,
+                              count: 3,
+                              effect: SlideEffect(
+                                activeDotColor: Style.primaryColor,
+                                spacing: 14.0,
+                                radius: 8.0,
+                                dotWidth: 16,
+                                dotHeight: 16,
+                                dotColor: Style.primaryColor30,
+                              ),
+                            )
+                          : Container(),
+                    ),
+                  ),
+
+                  // Arrow button
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Style.primaryColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios_rounded,
+                          color: Colors.white),
+                      onPressed: () {
+                        controller.indicator.nextPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingWidgets extends StatelessWidget {
+  const OnBoardingWidgets({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 70),
+          height: Get.height * 0.65,
+          child: Image.asset(
+            'assets/images/onboard1.png',
+            scale: 1.75,
+            fit: BoxFit
+                .contain, // bisa juga coba BoxFit.cover tergantung kebutuhan
+          ),
+        ),
+        Container(
+          height: Get.height * 0.35,
+          width: Get.width,
+          padding: EdgeInsets.only(bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Jelajah Seru, Bumi Lebih Asri',
+                  style: Style.headLineStyle2),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Liburan tetap seru, tapi punya dampak positif\nbagi lingkungan',
+                style: Style.headLineStyle5,
+                textAlign: TextAlign.center,
               )
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              //Skip button
-              GestureDetector(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Skip",
-                    style: Style.headLineStyle5,
-                  ),
-                ),
-              ),
-              SmoothPageIndicator(
-                controller: controller.indicator,
-                count: 3,
-                effect: SlideEffect(
-                    activeDotColor: Style.primaryColor,
-                    spacing: 8.0,
-                    radius: 8.0,
-                    dotWidth: 12,
-                    dotHeight: 12,
-                    dotColor: Style.primaryColor30),
-              ),
-              // Circle Arrow Button
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Style.primaryColor,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    icon: const Icon(Icons.arrow_forward, color: Colors.white),
-                    onPressed: () {
-                      // Handle next
-                    },
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
