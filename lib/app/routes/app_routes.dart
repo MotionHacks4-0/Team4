6f346bf4239eba043a0be956dd961fb6667febf4
VeironVaya <veironvayayarief41@gmail.com>
2025-04-30 10:36:11 +0700

    Login proggress-1 dan debugging gradle SDK 23

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,6 +6,8 @@ abstract class Routes {
   static const splash = _Paths.splash;
   static const HOME = _Paths.HOME;
   static const ONBOARDING = _Paths.ONBOARDING;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
 }
 
 abstract class _Paths {
@@ -13,4 +15,6 @@ abstract class _Paths {
   static const splash = '/splash';
   static const HOME = '/home';
   static const ONBOARDING = '/onboarding';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
 }
