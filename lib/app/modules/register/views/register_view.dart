81c23b2d59b0ceff91e5e66d67be2a4b3f9be590
VeironVaya <veironvayayarief41@gmail.com>
2025-05-02 18:01:48 +0700

    FINAL DONE

--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -15,95 +15,102 @@ class RegisterView extends GetView<RegisterController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Style.whiteColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(right: 40),
-              child: Text(
-                "Ayo gabung bersama kami,",
-                style: Style.headLineStyle2,
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: EdgeInsets.only(right: 140),
-              child: Text(
-                "Masukkan data diri anda",
-                style: Style.headLineStyle9,
-              ),
-            ),
-            SizedBox(height: 28),
-            CustomTextform(
-              title: "Nama",
-              textHint: "Masukkan nama anda",
-            ),
-            SizedBox(height: 22),
-            CustomTextform(
-              title: "Email",
-              textHint: "Masukkan email anda",
-              onChanged: (value) => controller.email.value = value,
-            ),
-            SizedBox(height: 22),
-            Obx(
-              () => CustomTextform(
-                title: "Kata Sandi",
-                textHint: "Buat kata sandi",
-                icon: "eye.png",
-                obscureText: controller.isPasswordHidden.value,
-                onIconTap: controller.togglePasswordVisibility,
-                onChanged: (value) => controller.password.value = value,
-              ),
-            ),
-            SizedBox(height: 37),
-            CustomButton(
-              text: "Daftar",
-              onPress: () => controller.register(),
-              textStyle: Style.headLineStyle6,
-              col: Style.primaryColor,
-              borderColor: Style.primaryColor,
-            ),
-            SizedBox(height: 16),
-            Text(
-              "Atau",
-              style: Style.headLineStyle7,
-            ),
-            SizedBox(height: 16),
-            CustomButton(
-              text: "Masuk dengan Google",
-              onPress: () {
-                controller.loginWithGoogle();
-              },
-              icon: "google.png",
-              col: Style.whiteColor,
-              borderColor: Style.greyColor1,
-              textStyle: Style.headLineStyle5,
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Sudah memiliki akun? ",
-                  style: Style.headLineStyle7,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Get.offNamed(Routes.LOGIN);
-                  },
-                  child: Text(
-                    "Masuk",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 120),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 40),
+                    child: Text(
+                      "Ayo gabung bersama kami,",
+                      style: Style.headLineStyle2,
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.only(right: 140),
+                    child: Text(
+                      "Masukkan data diri anda",
+                      style: Style.headLineStyle9,
+                    ),
+                  ),
+                  SizedBox(height: 28),
+                  CustomTextform(
+                    title: "Nama",
+                    textHint: "Masukkan nama anda",
+                  ),
+                  SizedBox(height: 22),
+                  CustomTextform(
+                    title: "Email",
+                    textHint: "Masukkan email anda",
+                    onChanged: (value) => controller.email.value = value,
+                  ),
+                  SizedBox(height: 22),
+                  Obx(
+                    () => CustomTextform(
+                      title: "Kata Sandi",
+                      textHint: "Buat kata sandi",
+                      icon: "eye.png",
+                      obscureText: controller.isPasswordHidden.value,
+                      onIconTap: controller.togglePasswordVisibility,
+                      onChanged: (value) => controller.password.value = value,
+                    ),
+                  ),
+                  SizedBox(height: 37),
+                  CustomButton(
+                    text: "Daftar",
+                    onPress: () => controller.register(),
+                    textStyle: Style.headLineStyle6,
+                    col: Style.primaryColor,
+                    borderColor: Style.primaryColor,
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "Atau",
+                    style: Style.headLineStyle7,
+                  ),
+                  SizedBox(height: 16),
+                  CustomButton(
+                    text: "Masuk dengan Google",
+                    onPress: () {
+                      controller.loginWithGoogle();
+                    },
+                    icon: "google.png",
+                    col: Style.whiteColor,
+                    borderColor: Style.greyColor1,
+                    textStyle: Style.headLineStyle5,
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Sudah memiliki akun? ",
+                        style: Style.headLineStyle7,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Get.offNamed(Routes.LOGIN);
+                        },
+                        child: Text(
+                          "Masuk",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
